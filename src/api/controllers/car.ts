50fc162b135ba996968b8b0299b955b3c9aded06
afeefazaman <engr.afeefa@gmail.com>
2023-01-30 05:59:05 -0600

    Car controller - Post end point added

--- src/api/controllers/car.ts
+++ src/api/controllers/car.ts
@@ -1,8 +1,11 @@
 import { getRepository } from "typeorm";
-import { Get, JsonController, Param, UseInterceptor } from "routing-controllers";
+import {Body, Get, JsonController, Param,Post, UseInterceptor } from "routing-controllers";
 
 import { Car } from "../models";
+import { CarDTO } from "./../interfaces";
 import { CarInterceptor } from "../interceptors";
+import { VinDecoder } from "./utils/vinDecoder";
+
 
 @JsonController("/car")
 @UseInterceptor(CarInterceptor)
@@ -15,5 +18,17 @@ export class CarController {
   @Get("/:id")
   async getById(@Param("id") id: string) {
     return await getRepository(Car).findOne(id);
+  }
+
+  @Post()
+  async create(@Body() body: CarDTO) {
+    const vinDecodedDetails = await VinDecoder(body.vin);
+    const { Make, Model, ModelYear } = vinDecodedDetails;
+    return getRepository(Car).save({
+      make: Make || null,
+      model: Model || null,
+      year: ModelYear || null,
+      ...body,
+    });
   }
 }
