b59062713ced234a27ab22899bdad6a990ac1914
afeefazaman <engr.afeefa@gmail.com>
2023-01-30 05:56:42 -0600

    Car controller - Get end point added

--- src/api/controllers/car.ts
+++ src/api/controllers/car.ts
@@ -1,7 +1,14 @@
-import { JsonController, UseInterceptor } from "routing-controllers";
+import { getRepository } from "typeorm";
+import { Get, JsonController, UseInterceptor } from "routing-controllers";
 
+import { Car } from "../models";
 import { CarInterceptor } from "../interceptors";
 
 @JsonController("/car")
 @UseInterceptor(CarInterceptor)
-export class CarController {}
+export class CarController {
+  @Get()
+  async get() {
+    return getRepository(Car).find();
+  }
+}
