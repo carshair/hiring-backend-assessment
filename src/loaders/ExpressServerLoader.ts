9b1eee6d3655fd78860a1c9743fec1fe93b84695
afeefazaman <engr.afeefa@gmail.com>
2023-01-30 05:52:40 -0600

    Middlewares - Custom error handler added

--- src/loaders/ExpressServerLoader.ts
+++ src/loaders/ExpressServerLoader.ts
@@ -2,13 +2,14 @@ import { Application } from "express";
 import { createExpressServer } from "routing-controllers";
 
 import * as Controllers from "../api/controllers";
+import * as MiddleWares from "../api/middlewares";
 
 export function ExpressServerLoader(): Application {
   const expressApp: Application = createExpressServer({
     cors: true,
     classTransformer: true,
     defaultErrorHandler: false,
-    middlewares: [],
+    middlewares: Object.values(MiddleWares),
     controllers: Object.values(Controllers),
   });
 
